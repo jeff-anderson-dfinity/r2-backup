5b86963688396c759f2896aa51933f5e2b941bf0
Jose Moran Urena <jose.moranurena.official@gmail.com>
2023-09-10 09:26:44 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,26 @@
 #!/bin/sh -l
 set -euo pipefail
 
-# Check mirror target is set
 if [ -z "$BUCKET_NAME" ]; then
   echo "BUCKET_NAME is not set."
   exit 1
 fi
 
-# Check if account ID is set
 if [ -z "$ACCOUNT_ID" ]; then
   echo "ACCOUNT_ID is not set."
   exit 1
 fi
 
+if [ -z "$ACCESS_KEY_ID" ]; then
+  echo "ACCESS_KEY_ID is not set."
+  exit 1
+fi
+
+if [ -z "$SECRET_ACCESS_KEY" ]; then
+  echo "SECRET_ACCESS_KEY is not set."
+  exit 1
+fi
+
 export MC_HOST_s3=https://${ACCESS_KEY_ID}:${SECRET_ACCESS_KEY}@${ACCOUNT_ID}.r2.cloudflarestorage.com
 
-# Execute mc mirror
 mc mirror $* "." "s3/$BUCKET_NAME"
